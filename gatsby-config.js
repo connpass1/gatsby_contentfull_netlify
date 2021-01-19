4348f1376d03f9033a10012ceed9fe63b7f9a067
connpass1 <connpass@gmail.com>
2021-01-19 12:40:00 +0300

    first

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,10 @@ const contentfulConfig = {
   spaceId: process.env.CONTENTFUL_SPACE_ID,
   accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
 }
-
+// const contentfulConfig = {
+//   spaceId: process.env.CONTENTFUL_SPACE_ID,
+//   accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+// }
 // if you want to use the preview API please define
 // CONTENTFUL_HOST in your environment config
 // the `host` property should map to `preview.contentful.com`
